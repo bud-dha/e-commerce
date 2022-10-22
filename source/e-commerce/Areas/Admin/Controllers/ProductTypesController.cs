7cf0bd7c034a04391f3d3dd049598a56afd2172f
Nikita Martsenkovsky <martsenkovskyn@gmail.com>
2022-10-22 23:28:15 +0700

    Добавил методы редактирования, удаления, получения инфо. о типе продукта.

--- source/e-commerce/Areas/Admin/Controllers/ProductTypesController.cs
+++ source/e-commerce/Areas/Admin/Controllers/ProductTypesController.cs
@@ -23,6 +23,7 @@ namespace e_commerce.Areas.Admin.Controllers
             return View(_db.ProductTypes.ToList());
         }
 
+        #region Создание типа продукта.
         //Create Get action Method
         public ActionResult Create()
         {
@@ -44,7 +45,9 @@ namespace e_commerce.Areas.Admin.Controllers
 
             return View(productTypes);
         }
+        #endregion
 
+        #region Редактирование типа продукта.
         //Edit Get action Method
         public ActionResult Edit(int? id)
         {
@@ -74,8 +77,81 @@ namespace e_commerce.Areas.Admin.Controllers
                 TempData["save"] = "Product type has been saved";
                 return RedirectToAction(nameof(Index));
             }
+            return View(productTypes);
+        }
+        #endregion
+
+        #region Получение информации о типе продукта.
+        //Details Get action Method
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = _db.ProductTypes.Find(id);
+
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //Details Post action Method
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Details(ProductTypes productTypes)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
+        #region Удаление типа продукта
+        //Delete Get action Method
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = _db.ProductTypes.Find(id);
+
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //Delete Post action Method
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Delete(int? id, ProductTypes productTypes)
+        {
+            var productType = _db.ProductTypes.Find(id);
+
+            if (id == null)           
+                return NotFound();
+
+            if (id != productTypes.Id)
+                return NotFound();    
+            
+            if (productType == null)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _db.ProductTypes.Remove(productType);
+                await _db.SaveChangesAsync();
+                TempData["save"] = "Product type has been saved";
+                return RedirectToAction(nameof(Index));
+            }
 
             return View(productTypes);
         }
+        #endregion
     }
 }
--- source/e-commerce/Startup.cs
+++ source/e-commerce/Startup.cs
@@ -26,9 +26,7 @@ namespace e_commerce
         {
             services.AddControllersWithViews();
 
-            services.AddDbContext<ApplicationContext>(options =>
-               options.UseSqlServer(
-                   Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,18 +42,16 @@ namespace e_commerce
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "areas",
-                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+                name: "areas",
+                pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
